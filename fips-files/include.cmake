392f70def4bdc308cf2d45b999d5f7f5e11ee0a9
Johannes Hirche <johannes.hirche@gmail.com>
2020-12-20 23:26:34 +0100

    - updated linux binary - improved flatc detection on other platforms

--- fips-files/include.cmake
+++ fips-files/include.cmake
@@ -2,9 +2,17 @@
 #   Wrap flatc code generation
 #
 
+if(FIPS_LINUX)
+    set(folder linux)
+elseif(FIPS_MACOS)
+    set(folder osx)
+else()
+    set(folder win32)
+endif()
+
 FIND_PROGRAM(FLATC
        NAMES flatc.exe flatc
-       PATHS ${FIPS_PROJECT_DIR}/../fips-flatbuffers/tools/win32
+       PATHS ${FIPS_PROJECT_DIR}/../fips-flatbuffers/tools/${folder}
        NO_DEFAULT_PATH
        )
 
@@ -15,7 +23,7 @@ macro(flatc)
         set(target_has_flatc 1)
         get_filename_component(f_abs ${CurDir}${fb} ABSOLUTE)
         get_filename_component(f_dir ${f_abs} PATH)        
-        STRING(REPLACE ".fbs" "_generated.h" out_header ${fb})
+        STRING(REPLACE ".fbs" ".h" out_header ${fb})
         STRING(FIND "${CMAKE_CURRENT_SOURCE_DIR}"  "/" last REVERSE)
         STRING(SUBSTRING "${CMAKE_CURRENT_SOURCE_DIR}" ${last}+1 -1 folder)
         set(abs_output_folder "${CMAKE_BINARY_DIR}/flatbuffer/${CurTargetName}/${CurDir}")
@@ -23,7 +31,7 @@ macro(flatc)
         set(output ${abs_output_folder}/${out_header})
         
         add_custom_command(OUTPUT ${output}
-                PRE_BUILD COMMAND ${FLATC} -c --scoped-enums --cpp-str-flex-ctor --cpp-str-type Util::String -o "${abs_output_folder}" "${f_abs}" 
+                PRE_BUILD COMMAND ${FLATC} -c --gen-object-api --gen-compare --scoped-enums --gen-mutable --cpp-str-flex-ctor --cpp-str-type Util::String --filename-suffix "" -o "${abs_output_folder}" "${f_abs}" 
                 MAIN_DEPENDENCY ${f_abs}
                 DEPENDS ${FLATC}
                 WORKING_DIRECTORY ${FIPS_PROJECT_DIR}
